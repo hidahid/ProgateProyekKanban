d6a558206e6a3e3dfaf5d9ddfb70a81d3e6d66cb
hidahid <ahid.abdurrahman.saleh@gmail.com>
2023-07-04 19:08:40 +0700

    Install Recoil and develop task summary page

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,12 @@ import React from 'react'
 export interface CSSProperties {
   [key: string]: React.CSSProperties
 }
+
+// Ditambahkan
+export interface Task {
+  id: number
+  title: string
+  detail: string
+  dueDate: string
+  progressOrder: number
+}
