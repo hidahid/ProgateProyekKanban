e3a8e5a9edab6f64708f9e792476d58b0a3bc64c
hidahid <ahid.abdurrahman.saleh@gmail.com>
2023-07-11 20:41:22 +0700

    Refactor completeTask function

--- src/features/tasks/components/TaskList/TaskListItem.tsx
+++ src/features/tasks/components/TaskList/TaskListItem.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+// Hapus import {useRecoilState} from 'recoil'
+// Hapus import {tasksState} from '../../TaskAtoms'
+// import { useRecoilState } from 'recoil'
+// import { tasksState } from '../../TaskAtoms'
 import type { Task, CSSProperties } from '../../../../types'
 import { TASK_PROGRESS_STATUS, TASK_PROGRESS_ID } from '../../../../constants/app'
+import { useTasksAction } from '../../hooks/Tasks' // Ditambahkan
 
 interface TaskListItemProps {
   task: Task
+}
+
+// Definisikan function ini
+const getIconStyle = (progressOrder: number): React.CSSProperties => {
+  const color: '#55C89F' | '#C5C5C5' =
+    progressOrder === TASK_PROGRESS_ID.COMPLETED ? '#55C89F' : '#C5C5C5'
+
+  const cursor: 'default' | 'pointer' =
+    progressOrder === TASK_PROGRESS_ID.COMPLETED ? 'default' : 'pointer'
+
+  return {
+    color,
+    cursor,
+    fontSize: '28px',
+    marginRight: '6px',
+  }
 }
 
 // Ditambahkan
@@ -23,10 +44,32 @@ const getProgressCategory = (progressOrder: number): string => {
 }
 
 const TaskListItem = ({ task }: TaskListItemProps): JSX.Element => {
+  // Hapus const [tasks, setTasks] = useRecoilState<Task[]>(tasksState)
+  // Hapus const completeTask = (taskId: number): void => {...}
+
+  // const [tasks, setTasks] = useRecoilState<Task[]>(tasksState)
+
+  // const completeTask = (taskId: number): void => {
+  //   const updatedTasks: Task[] = tasks.map((task) =>
+  //     task.id === taskId ? { ...task, progressOrder: TASK_PROGRESS_ID.COMPLETED } : task
+  //   )
+  //   setTasks(updatedTasks)
+  // }
+
+  const { completeTask } = useTasksAction() // Ditambahkan
+
   return (
     <div style={styles.tableBody}>
       <div style={styles.tableBodyTaskTitle}>
-        <span className="material-icons">check_circle</span>
+        <span
+          className="material-icons"
+          style={getIconStyle(task.progressOrder)}
+          onClick={() => {
+            completeTask(task.id)
+          }}
+        >
+          check_circle
+        </span>
         {task.title}
       </div>
       <div style={styles.tableBodyDetail}>{task.detail}</div>
--- src/features/tasks/components/TaskProgress/TaskCard.tsx
+++ src/features/tasks/components/TaskProgress/TaskCard.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+// Hapus import {useRecoilState} from 'recoil'
+// Hapus import {tasksState} from '../../TaskAtoms'
+// import { useRecoilState } from 'recoil'
+// import { tasksState } from '../../TaskAtoms'
 import type { Task, CSSProperties } from '../../../../types'
 import { TASK_PROGRESS_ID } from '../../../../constants/app'
+import { useTasksAction } from '../../hooks/Tasks' // Ditambahkan
 
 interface TaskCardProps {
   task: Task
+}
+
+// Definisikan function ini
+const getIconStyle = (progressOrder: number): React.CSSProperties => {
+  const color: '#55C89F' | '#C5C5C5' =
+    progressOrder === TASK_PROGRESS_ID.COMPLETED ? '#55C89F' : '#C5C5C5'
+
+  const cursor: 'default' | 'pointer' =
+    progressOrder === TASK_PROGRESS_ID.COMPLETED ? 'default' : 'pointer'
+
+  return {
+    color,
+    cursor,
+    fontSize: '28px',
+  }
 }
 
 const getArrowPositionStyle = (progressOrder: number): React.CSSProperties => {
@@ -17,10 +37,33 @@ const getArrowPositionStyle = (progressOrder: number): React.CSSProperties => {
 }
 
 const TaskCard = ({ task }: TaskCardProps): JSX.Element => {
+  // Hapus const [tasks, setTasks] = useRecoilState<Task[]>(tasksState)
+  // Hapus const completeTask = (taskId: number): void => {...}
+
+  // const [tasks, setTasks] = useRecoilState<Task[]>(tasksState)
+
+  // const completeTask = (taskId: number): void => {
+  //   const updatedTasks: Task[] = tasks.map((task) =>
+  //     task.id === taskId ? { ...task, progressOrder: TASK_PROGRESS_ID.COMPLETED } : task
+  //   )
+  //   setTasks(updatedTasks)
+  // }
+
+  const { completeTask } = useTasksAction() // Ditambahkan
+
   return (
     <div style={styles.taskCard}>
       <div style={styles.taskIcons}>
-        <div className="material-icons">check_circle</div>
+        {/* Diperbarui */}
+        <div
+          className="material-icons"
+          style={getIconStyle(task.progressOrder)}
+          onClick={(): void => {
+            completeTask(task.id) // Ditambahkan
+          }}
+        >
+          check_circle
+        </div>
         <div className="material-icons" style={styles.menuIcon}>
           more_vert
         </div>
